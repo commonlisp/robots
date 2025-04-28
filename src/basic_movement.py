bed7fc7c413f2d2d6c8a80b993c89b50b7b2608a
George Kuan <commonlisp@users.noreply.github.com>
2025-04-28 14:46:22 -0500

    Update basic_movement.py

--- src/basic_movement.py
+++ src/basic_movement.py
@@ -5,11 +5,15 @@ import runloop
 WheelCircumferenceCm = 27.6
 
 def degreesFromDistanceCentimeters(distanceCm):
-    return int(distanceCm*WheelCircumferenceCm/360)
+    return int(round(distanceCm*360/WheelCircumferenceCm))
 
 async def main():
     await light_matrix.write("Hi!")
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
-	velocity = 100
-	# Move forward for given distance
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, degreesFromDistanceCentimeters(40), velocity, velocity)
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+    velocity = 100
+    # Move forward for given distance
+    degrees = degreesFromDistanceCentimeters(40)
+    print(degrees)
+    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, degrees, velocity, velocity)
+
+runloop.run(main())
