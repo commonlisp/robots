5627f8efb2e3ee44f9d80a93f24421ee16769350
George Kuan <commonlisp@users.noreply.github.com>
2025-04-28 14:28:31 -0500

    movement

--- src/basic_movement.py
+++ src/basic_movement.py
@@ -9,7 +9,7 @@ def degreesFromDistanceCentimeters(distanceCm):
 
 async def main():
     await light_matrix.write("Hi!")
-    await motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+    motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
 	velocity = 100
 	# Move forward for given distance
     await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, degreesFromDistanceCentimeters(40), velocity, velocity)
